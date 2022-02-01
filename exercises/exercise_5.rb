f83258ee700479b204a5b086c729484e749fe405
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-01 12:52:27 -0700

    Improve wording of variable names and outputs

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -9,7 +9,7 @@ puts "----------"
 
 # Your code goes here ...
 
-@gross_total_revenue = Store.sum("annual_revenue")
-@average_aggregate_annual_revenue = @gross_total_revenue / Store.count
-puts "The total revenue for all locations is: $" + @gross_total_revenue.to_s
-puts "The total average revenue for the entire company is: $" + @average_aggregate_annual_revenue.to_s
+@aggregate_total_revenue = Store.sum("annual_revenue")
+@average_aggregate_annual_revenue = @aggregate_total_revenue / Store.count
+puts "The total revenue for the entire company is: $" + @aggregate_total_revenue.to_s
+puts "The average revenue for the entire company is: $" + @average_aggregate_annual_revenue.to_s
