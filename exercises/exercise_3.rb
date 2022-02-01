978e5bbcf904efaf1ab6edf15bcee6a43c550f52
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-01 12:22:34 -0700

    Delete store with id 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+
+puts "Current number of stores: " + Store.count.to_s
+
+@store3.destroy
+
+puts "Current number of stores: " + Store.count.to_s
