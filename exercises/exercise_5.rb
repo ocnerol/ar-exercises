6c1c000debc2cc1ab046151c8b6e96a76eaeaeb0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-01 12:51:16 -0700

    Print average aggregate annual revenue for the company

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,4 +10,6 @@ puts "----------"
 # Your code goes here ...
 
 @gross_total_revenue = Store.sum("annual_revenue")
+@average_aggregate_annual_revenue = @gross_total_revenue / Store.count
 puts "The total revenue for all locations is: $" + @gross_total_revenue.to_s
+puts "The total average revenue for the entire company is: $" + @average_aggregate_annual_revenue.to_s
