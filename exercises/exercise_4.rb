bd97f5bdbb50c6cbfee4d1e032d95f51f94eeb5e
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-02-01 12:32:11 -0700

    Add 3 new stores and display name and annual revenue info for mens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,8 +2,20 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
+require 'pp'
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistley = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+@mens_stores.each do |store|
+  puts "The " + store.name + " location generates an annual revenue of " + store.annual_revenue.to_s + "."
+end
